927cd6e0534f2403f07a14ae6180955266396c4a
Maas Lalani <maas@lalani.dev>
2023-07-13 13:24:09 -0400

    feat: FindAllFiles, FindAllFilesExcept methods for ignoring .gitignore (#4)

--- gitcha_test.go
+++ gitcha_test.go
@@ -30,6 +30,52 @@ func TestGitRepoForPath(t *testing.T) {
 	}
 }
 
+func TestFindAllFiles(t *testing.T) {
+	tmp := t.TempDir()
+
+	gitignore, err := os.Create(filepath.Join(tmp, ".gitignore"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer gitignore.Close()
+	_, err = gitignore.WriteString("*.test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tt := []struct {
+		path string
+		list []string
+		exp  string
+	}{
+		{tmp, []string{"*.test"}, "ignore.test"},
+	}
+
+	for _, test := range tt {
+		f, err := os.Create(filepath.Join(tmp, test.exp))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+
+		ch, err := FindAllFiles(test.path, test.list)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var counter int
+		for v := range ch {
+			counter++
+			if test.exp != v.Info.Name() {
+				t.Errorf("Expected %v, got %v for %s", test.exp, v.Path, test.path)
+			}
+		}
+
+		if counter != 1 {
+			t.Errorf("Expected 1 file found, got %d for %s", counter, test.path)
+		}
+	}
+}
+
 func TestFindFiles(t *testing.T) {
 	tlink, err := tempLink(".")
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/muesli/gitcha
 
-go 1.14
+go 1.15
 
 require (
 	github.com/sabhiram/go-gitignore v0.0.0-20180611051255-d3107576ba94
